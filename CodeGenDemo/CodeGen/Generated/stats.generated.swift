dba435a0c0ffe3416d9678eb09e0f8cfd0c3142c
Olivier Halligon <olivier.halligon@niji.fr>
2017-09-19 17:22:19 +0200

    Step 2.2: Your first simplistic template to discover Sourcery

---
+++ CodeGenDemo/CodeGen/Generated/stats.generated.swift
@@ -0,0 +1,11 @@
+// Generated using Sourcery 0.8.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+/*
+This project contains:
+
+- 27 enums
+- 9 structs
+- 8 classes
+- 2 protocols
+*/
