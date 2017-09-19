dc6fdbf0aa48675dfe9c3a2a3f3b1dcdb5036788
Olivier Halligon <olivier.halligon@niji.fr>
2017-09-19 20:13:31 +0200

    Step 6.2: Prepare our new template

---
+++ CodeGenDemo/CodeGen/Generated/AutoStringProperties.generated.swift
@@ -0,0 +1,26 @@
+// Generated using Sourcery 0.8.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+extension Person {
+  static let stringProperties: [String] = [
+    L10n.Person.firstName,
+    L10n.Person.lastName,
+  ]
+
+  subscript(propertyIndex idx: Int) -> String {
+    get {
+      switch idx {
+      case 0: return self.firstName
+      case 1: return self.lastName
+      default: fatalError("Out of bounds")
+      }
+    }
+    set {
+      switch idx {
+      case 0: self.firstName = newValue
+      case 1: self.lastName = newValue
+      default: fatalError("Out of bounds")
+      }
+    }
+  }
+}
