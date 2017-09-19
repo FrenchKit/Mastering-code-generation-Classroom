b3b12d85368c227233c1ddf2dfc2f3a9172334ab
Olivier Halligon <olivier.halligon@niji.fr>
2017-09-19 20:34:47 +0200

    Step 6.6: Move our test loops into the code we want to generate to get closer

--- CodeGenDemo/CodeGen/Generated/AutoStringProperties.generated.swift
+++ CodeGenDemo/CodeGen/Generated/AutoStringProperties.generated.swift
@@ -2,16 +2,12 @@
 // DO NOT EDIT
 
 
- - Address
-   -> 1: street
-   -> 2: city
-   -> 3: state
- - Person
-   -> 1: firstName
-   -> 2: lastName
 
-extension Person {
+extension Address {
   static let stringProperties: [String] = [
+    -> 1: street
+    -> 2: city
+    -> 3: state
     L10n.Person.firstName,
     L10n.Person.lastName,
   ]
@@ -19,6 +15,9 @@ extension Person {
   subscript(propertyIndex idx: Int) -> String {
     get {
       switch idx {
+      -> 1: street
+      -> 2: city
+      -> 3: state
       case 0: return self.firstName
       case 1: return self.lastName
       default: fatalError("Out of bounds")
@@ -26,6 +25,39 @@ extension Person {
     }
     set {
       switch idx {
+      -> 1: street
+      -> 2: city
+      -> 3: state
+      case 0: self.firstName = newValue
+      case 1: self.lastName = newValue
+      default: fatalError("Out of bounds")
+      }
+    }
+  }
+}
+
+extension Person {
+  static let stringProperties: [String] = [
+    -> 1: firstName
+    -> 2: lastName
+    L10n.Person.firstName,
+    L10n.Person.lastName,
+  ]
+
+  subscript(propertyIndex idx: Int) -> String {
+    get {
+      switch idx {
+      -> 1: firstName
+      -> 2: lastName
+      case 0: return self.firstName
+      case 1: return self.lastName
+      default: fatalError("Out of bounds")
+      }
+    }
+    set {
+      switch idx {
+      -> 1: firstName
+      -> 2: lastName
       case 0: self.firstName = newValue
       case 1: self.lastName = newValue
       default: fatalError("Out of bounds")
